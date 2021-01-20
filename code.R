3ab79da104f75e5618e0133c3341bc74173a0fe9
Miracle <73017979+RimjhimGupta@users.noreply.github.com>
2021-01-20 17:03:47 +0530

    Add R code

---
+++ code.R
@@ -0,0 +1,38 @@
+library(data.table)
+library(stringr)
+# data frames
+library(dplyr)
+
+# Reading the files
+main <- fread("E:/Program Files/R/R-4.0.3/intership-test-master/input/main.csv")
+# View(main)    
+
+filteredCountry <- main[like(COUNTRY,'USA')]
+# View(filteredCountry)
+
+print(nrow(main))
+print(nrow(filteredCountry ))
+fwrite(filteredCountry, "E:/Program Files/R/R-4.0.3/intership-test-master/output/filteredCountry.csv")
+
+filteredCountry <- fread("E:/Program Files/R/R-4.0.3/intership-test-master/output/filteredCountry.csv")
+
+lowestPrice1 <- data.table(filteredCountry,key = 'SKU')
+lowestPrice1 <- lowestPrice[,head(.SD,2),,by = c('SKU')]
+#View(lowestPrice1)
+
+lowestPrice1 <- lowestPrice1[, c('SKU', 'PRICE')]
+#View(lowestPrice1)
+first <- lowestPrice1[, .SD[1], by=c('SKU')]
+last <- lowestPrice1[, .SD[.N], by=c('SKU') ]
+last <- last[-1,]
+last <- last[-1,]
+
+setnames(first, c('PRICE'), c('FIRST_MINIMUM_PRICE'))
+setnames(last, c('PRICE'), c('SECOND_MINIMUM_PRICE'))
+
+
+lowestPrice <- merge(first, last, by=c('SKU'), all = TRUE)
+lowestPrice <- lowestPrice[, `SECOND_MINIMUM_PRICE` := ifelse(is.na(`SECOND_MINIMUM_PRICE`), 0, `SECOND_MINIMUM_PRICE`)]
+View(lowestPrice)
+fwrite(lowestPrice, "E:/Program Files/R/R-4.0.3/intership-test-master/output/lowestPrice.csv")
+
